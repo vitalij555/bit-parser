69222e37057b13987c4a73debe1a411fb81781c3
Vitalij Gortovskij <vitalij555@gmail.com>
2025-12-23 18:11:11 +0200

    Setup files updated and fixed

--- pypi/setup.py
+++ pypi/setup.py
@@ -1,32 +1,43 @@
+from pathlib import Path
+
 import setuptools
 from setuptools import setup
 
-with open("README.md", "r") as fh:
-  long_description = fh.read()
+ROOT_DIR = Path(__file__).resolve().parents[1]
+long_description = (ROOT_DIR / "README.md").read_text(encoding="utf-8")
 
 setup(
-  name = 'bit-parser',         # How you named your package folder (MyLib)
-  packages=setuptools.find_packages(),
-  version = '1.0.4',
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Parse binary data fields (bit maps, flag sets) represented as hex strings (helpful for parsing separate protocol elements found in trace files for example)',   # Give a short description about your library
-  long_description=long_description,
-  long_description_content_type="text/markdown",
-  author = 'Vitalij Gotovskij',               # Type in your name
-  author_email = 'vitalij@cdeveloper.eu',     # Type in your E-Mail
-  url = 'https://github.com/vitalij555/bit-parser',   # Provide either the link to your github or to your website
-  # download_url = 'https://github.com/vitalij555/event-notifier/archive/v_0_1_1.tar.gz',
-  keywords = ['EVENT', 'NOTIFY', 'SUBSCRIBE', "OBSERVER"],   # Keywords that define your package best
-  classifiers=[
-    'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'Operating System :: OS Independent',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3.10',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.11',
-    'Programming Language :: Python :: 3.12',
-  ],
+    name="bit-parser",
+    packages=setuptools.find_packages(where=str(ROOT_DIR)),
+    package_dir={"": str(ROOT_DIR)},
+    version="1.0.4",
+    license="MIT",
+    description="Parse compact bitfields (bitmaps, flag sets) represented as hex strings.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Vitalij Gotovskij",
+    author_email="vitalij@cdeveloper.eu",
+    url="https://github.com/vitalij555/bit-parser",
+    project_urls={
+        "Homepage": "https://github.com/vitalij555/bit-parser",
+        "Issues": "https://github.com/vitalij555/bit-parser/issues",
+    },
+    install_requires=[
+        "bitops",
+    ],
+    python_requires=">=3.8",
+    keywords=["bit", "bitfield", "bitmap", "protocol", "parser"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
 )
